1d4c4382bb631986b48c2cc7644e2b1d71bb01d7
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-11-18 16:57:28 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import bg from '/bg.jpg'
+
+const AppWrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    padding: 2rem;
+    background-image: url(${bg});
+    background-size: cover;
+`;
+
+const App = () => {
+
+  return (
+      <AppWrapper>
+          <h1>терминал оплаты мобильного телефона</h1>
+      </AppWrapper>
+  );
+};
+
+export default App;
